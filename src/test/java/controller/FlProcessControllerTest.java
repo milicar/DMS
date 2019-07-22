83380cf291a2b0c75afe6bc7b5d38371146c5579
Milica Rudic <milicarudic@gmail.com>
2019-07-22 22:13:27 +0200

    Tested FlProcessController

---
+++ src/test/java/controller/FlProcessControllerTest.java
@@ -0,0 +1,171 @@
+package controller;
+
+import com.mr.service.ProcessService;
+import com.mr.controller.FlProcessController;
+import com.mr.controller.LoginController;
+import com.mr.domain.Activity;
+import com.mr.domain.Company;
+import com.mr.domain.FirstLevelProcess;
+import com.mr.domain.Process;
+import com.mr.domain.Role;
+import com.mr.domain.Subprocess;
+import com.mr.domain.User;
+import com.mr.service.ActivityService;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FlProcessControllerTest {
+
+    Company company;
+    Process process;
+    Process subprocess;
+    List<Process> processesList;
+    List<Activity> activitiesList;
+    @Mock
+    ProcessService processService;
+    @Mock
+    ActivityService activityService;
+    @Mock
+    LoginController loginController;
+    @InjectMocks
+    FlProcessController flProcessControler;
+
+    @Test
+    public void shouldReturnAllProcessesForCompany() {
+        company = new Company();
+        process = new FirstLevelProcess();
+        processesList = new ArrayList<>();
+        processesList.add(process);
+        when(processService.findAllFor(company)).thenReturn(processesList);
+
+        flProcessControler.showAllFor(company);
+
+        verify(processService).findAllFor(company);
+        assertEquals(company, flProcessControler.getParentCompany());
+        assertEquals(processesList, flProcessControler.getProcesses());
+        assertEquals("list_flprocesses", flProcessControler.showAllFor(company));
+    }
+
+    @Test
+    public void shouldReturnCompanyProcessesForEmployee() {
+        User user = new User();
+        user.setUserRole(Role.USER);
+        process = new FirstLevelProcess();
+        processesList = new ArrayList<>();
+        processesList.add(process);
+        when(loginController.getFlProcessesForUser()).thenReturn(processesList);
+
+        flProcessControler.showAllFor(user);
+
+        assertEquals(processesList, flProcessControler.getProcesses());
+        assertEquals("list_flprocesses", flProcessControler.showAllFor(user));
+    }
+
+    @Test
+    public void shouldReturnProcessesForAllCompaniesForAdmin() {
+        User admin = new User();
+        admin.setUserRole(Role.ADMIN);
+        process = new FirstLevelProcess();
+        processesList = new ArrayList<>();
+        processesList.add(process);
+        when(processService.findAll()).thenReturn(processesList);
+
+        flProcessControler.showAllFor(admin);
+
+        verify(processService).findAll();
+        assertEquals(processesList, flProcessControler.getProcesses());
+        assertEquals("list_flprocesses", flProcessControler.showAllFor(admin));
+    }
+
+    @Test
+    public void shouldCreateNewFlProcessForACompany() {
+        company = new Company();
+
+        flProcessControler.createNewFor(company);
+
+        assertEquals(company, flProcessControler.getParentCompany());
+        assertNotNull(flProcessControler.getFlProcess());
+        assertEquals("flprocess_form", flProcessControler.createNewFor(company));
+
+    }
+
+    @Test
+    public void shouldShowOneProcessWithSubprocesses() {
+        process = new FirstLevelProcess();
+        subprocess = new Subprocess();
+        processesList = new ArrayList<>();
+        processesList.add(subprocess);
+        when(processService.findAllFor(process)).thenReturn(processesList);
+
+        flProcessControler.show((FirstLevelProcess) process);
+
+        verify(processService).findAllFor(process);
+        verify(activityService).findAllFor(process);
+        assertEquals(process, flProcessControler.getFlProcess());
+        assertEquals(processesList, flProcessControler.getProcessChildren());
+        assertTrue(flProcessControler.getActivityChildren().isEmpty());
+        assertEquals("flprocess", flProcessControler.show((FirstLevelProcess) process));
+    }
+
+    @Test
+    public void shouldShowOneProcessWithItsActivities() {
+        process = new FirstLevelProcess();
+        Activity activity = new Activity();
+        activitiesList = new ArrayList<>();
+        activitiesList.add(activity);
+        when(activityService.findAllFor(process)).thenReturn(activitiesList);
+
+        flProcessControler.show((FirstLevelProcess) process);
+
+        verify(processService).findAllFor(process);
+        verify(activityService).findAllFor(process);
+        assertEquals(process, flProcessControler.getFlProcess());
+        assertTrue(flProcessControler.getProcessChildren().isEmpty());
+        assertEquals(activitiesList, flProcessControler.getActivityChildren());
+        assertEquals("flprocess", flProcessControler.show((FirstLevelProcess) process));
+    }
+
+    @Test
+    public void shouldSaveProcess() {
+        process = new FirstLevelProcess();
+        flProcessControler.setFlProcess((FirstLevelProcess) process);
+        company = new Company();
+        flProcessControler.setParentCompany(company);
+
+        flProcessControler.save();
+
+        verify(processService).save(process);
+        assertEquals(company, flProcessControler.getFlProcess().getParent());
+    }
+    
+    @Test
+    public void shouldEditProcess() {
+        process = new FirstLevelProcess();
+        
+        flProcessControler.edit((FirstLevelProcess) process);
+        
+        assertEquals(process, flProcessControler.getFlProcess());
+        assertEquals("flprocess_form", flProcessControler.edit((FirstLevelProcess) process));
+    }
+    
+    @Test
+    public void shouldDeleteProcess() {
+        process = new FirstLevelProcess();
+                
+        flProcessControler.delete((FirstLevelProcess) process);
+        
+        verify(processService).delete(process);
+    }
+}
